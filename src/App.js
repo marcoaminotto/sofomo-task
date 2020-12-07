6edcb366178db53abacc0b7c1ed9288a6b319015
Marco Echevestre <marcoaminotto@hotmail.com>
2020-12-07 19:10:02 +0100

    feat: create carousel base

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
+import Carousel from './containers/Carousel';
+
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">
+    <Carousel />
+  </div>;
 }
 
 export default App;
---
+++ src/containers/Carousel/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Carousel = () => {
+  return (
+    <div id="carousel">
+      {/* <Slider />
+      <Filmtrip />
+      <ArrowButton /> right
+      <ArrowButton /> left */}
+    </div>
+  );
+};
+
+export default Carousel;
