da1208ba1cb4a5ab95a7cda0a43299fee1467077
Marco Echevestre <marcoaminotto@hotmail.com>
2020-12-07 19:49:05 +0100

    feat: add buttons to carousel

--- src/containers/Carousel/index.js
+++ src/containers/Carousel/index.js
@@ -1,9 +1,16 @@
 import React, { useState } from 'react';
 
+import ArrowButton from '../../components/ArrowButton';
 import Image from '../../components/Image';
 
 const Carousel = () => {
   const [imagesList, setImagesList] = useState([1, 2, 3, 4]);
+
+  const nextImage = () => {
+  };
+
+  const prevImage = () => {
+  };
 
   return (
     <div id="carousel">
@@ -13,6 +20,8 @@ const Carousel = () => {
           return <Image key={index} src={imagePath} />;
         })}
       </div>
+      <ArrowButton direction="left" handleClick={prevImage} />
+      <ArrowButton direction="right" handleClick={nextImage} />
       {/*
       <Filmtrip />
       <ArrowButton /> right
