1cea8b68d16b6dac44ee06ddee43efb65e9ab413
Marco Echevestre <marcoaminotto@hotmail.com>
2020-12-07 19:59:21 +0100

    feat: create slider infinite effect

--- src/containers/Carousel/index.js
+++ src/containers/Carousel/index.js
@@ -5,16 +5,45 @@ import Image from '../../components/Image';
 
 const Carousel = () => {
   const [imagesList, setImagesList] = useState([1, 2, 3, 4]);
+  const [translate, setTranslate] = useState(0);
+  const [transition, setTransition] = useState(0.45);
+  const [activeImage, setActiveImage] = useState(0);
+
+  const getWindowWidth = () => {
+    return window.innerWidth;
+  };
+
+  const imagesTransition = {
+    transform: `translateX(${translate}px)`,
+    transition: `transform ease-out ${transition}s`,
+    width: `${getWindowWidth() * imagesList.length}px`,
+  };
 
   const nextImage = () => {
+    if (activeImage * -1 === imagesList.length - 1) {
+      setTranslate(0);
+      setActiveImage(0);
+      return;
+    }
+
+    setActiveImage(activeImage - 1);
+    setTranslate((activeImage - 1) * getWindowWidth());
   };
 
   const prevImage = () => {
+    if (activeImage === 0) {
+      setTranslate((imagesList.length - 1) * getWindowWidth() * -1);
+      setActiveImage((imagesList.length - 1) * -1);
+      return;
+    }
+
+    setActiveImage(activeImage + 1);
+    setTranslate((activeImage + 1) * getWindowWidth());
   };
 
   return (
     <div id="carousel">
-      <div id="slider">
+      <div id="slider" style={imagesTransition}>
         {imagesList.map((index) => {
           const imagePath = `/images/image${index}.jpg`;
           return <Image key={index} src={imagePath} />;
@@ -23,9 +52,7 @@ const Carousel = () => {
       <ArrowButton direction="left" handleClick={prevImage} />
       <ArrowButton direction="right" handleClick={nextImage} />
       {/*
-      <Filmtrip />
-      <ArrowButton /> right
-      <ArrowButton /> left */}
+      <Filmtrip />*/}
     </div>
   );
 };
