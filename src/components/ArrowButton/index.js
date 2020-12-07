eae0b959b1da0622b7f73308080d982597bf2849
Marco Echevestre <marcoaminotto@hotmail.com>
2020-12-07 19:46:32 +0100

    feat: create arrow buttons

---
+++ src/components/ArrowButton/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const ArrowButton = ({ direction, handleClick }) => {
+
+  const buttonClasses = direction === 'right' ? 'right-direction' : 'left-direction';
+  const imgDirection = direction === 'right' ? 'img-right' : 'img-left';
+  const arrowImg = direction === 'right' ? '/icons/right-arrow.svg' : '/icons/left-arrow.svg';
+  const arrowDescription = direction === 'right' ? 'next' : 'back';
+
+  return (
+    <div className={`button ${buttonClasses}`} onClick={handleClick}>
+      <img className={imgDirection} src={arrowImg} alt={`${arrowDescription} advertising`} />
+    </div>
+  );
+};
+
+export default ArrowButton;
